6d6decb75aebef33d1c5c1c470ce40de50bce37b
Srikant Shubham <srikantsubham10000@gmail.com>
2023-06-16 14:43:20 +0530

    csrd origin checking

--- demo/settings.py
+++ demo/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
+CSRF_TRUSTED_ORIGINS = ['https://web-production-07f6.up.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
@@ -124,7 +124,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_HOST = "https://web-production-07f6.up.railway.app/" if not DEBUG else ""
+STATIC_URL = STATIC_HOST + "/static/"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 VENV_PATH = os.path.dirname(BASE_DIR)
